89dcdf114218f530296093a78116b08b0274e82e
DawnT112 <3193782263@qq.com>
2025-09-21 11:14:12 +0800

    FXXK U COMPILE

--- include/linux/mm_inline.h
+++ include/linux/mm_inline.h
@@ -200,7 +200,7 @@ static inline void lru_gen_update_size(struct lruvec *lruvec, struct page *page,
 	int type = page_is_file_cache(page);
 	int zone = page_zonenum(page);
 	int delta = hpage_nr_pages(page);
-	enum lru_list lru = type * LRU_INACTIVE_FILE;
+	enum lru_list lru = type * LRU_INACTive_FILE;
 	struct lru_gen_struct *lrugen = &lruvec->lrugen;
 
 	VM_BUG_ON(old_gen != -1 && old_gen >= MAX_NR_GENS);
@@ -317,7 +317,32 @@ static inline bool lru_gen_del_page(struct lruvec *lruvec, struct page *page, bo
 	return true;
 }
 
-// ！！！将这两个参数的函数移到 CONFIG_LRU_GEN 内部 ！！！
+#else
+
+static inline bool lru_gen_enabled(void)
+{
+	return false;
+}
+
+static inline bool lru_gen_in_fault(void)
+{
+	return false;
+}
+
+static inline bool lru_gen_add_page(struct lruvec *lruvec, struct page *page, bool reclaiming)
+{
+	return false;
+}
+
+static inline bool lru_gen_del_page(struct lruvec *lruvec, struct page *page, bool reclaiming)
+{
+	return false;
+}
+
+#endif /* CONFIG_LRU_GEN */
+
+// ！！！将两个参数的函数版本移到 CONFIG_LRU_GEN 之外，并用条件编译保护 ！！！
+#ifdef CONFIG_LRU_GEN
 static __always_inline void add_page_to_lru_list(struct page *page,
 				struct lruvec *lruvec)
 {
@@ -352,29 +377,24 @@ static __always_inline void del_page_from_lru_list(struct page *page,
 	update_lru_size(lruvec, page_lru(page), page_zonenum(page),
 			-hpage_nr_pages(page));
 }
-
 #else
-
-static inline bool lru_gen_enabled(void)
+static __always_inline void add_page_to_lru_list(struct page *page,
+				struct lruvec *lruvec)
 {
-	return false;
+	add_page_to_lru_list(page, lruvec, page_lru(page));
 }
 
-static inline bool lru_gen_in_fault(void)
+static __always_inline void add_page_to_lru_list_tail(struct page *page,
+				struct lruvec *lruvec)
 {
-	return false;
+	add_page_to_lru_list_tail(page, lruvec, page_lru(page));
 }
 
-static inline bool lru_gen_add_page(struct lruvec *lruvec, struct page *page, bool reclaiming)
+static __always_inline void del_page_from_lru_list(struct page *page,
+				struct lruvec *lruvec)
 {
-	return false;
+	del_page_from_lru_list(page, lruvec, page_lru(page));
 }
-
-static inline bool lru_gen_del_page(struct lruvec *lruvec, struct page *page, bool reclaiming)
-{
-	return false;
-}
-
 #endif /* CONFIG_LRU_GEN */
 
 #endif
